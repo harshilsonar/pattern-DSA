c10a76d8768d38e9858e0046379fcdb16949981d
harshilsonar <sonarhet8426@gmail.com>
2025-08-19 23:17:07 +0530

    update file

---
+++ pattern.js
@@ -0,0 +1,276 @@
+//q1
+for ( let i=1; i<=5; i++)
+{
+    for (let j=1;j<=i;j++)
+    {
+     document.writeln(j + " ")
+    }
+    document.writeln("<br>")
+}
+
+document.writeln("<br>")
+
+//q2
+for ( let i=1; i<=5; i++)
+{
+    for (let j=i;j>=1;j--)
+    {
+     document.writeln(j + " ")
+    }
+    document.writeln("<br>")
+}
+
+document.writeln("<br>")
+
+
+//q3
+for ( let i=1; i<=5; i++)
+{
+    for (let j=1;j<=i;j++)
+    {
+     document.writeln(i + " ")
+    }
+    document.writeln("<br>")
+}
+
+document.writeln("<br>")
+
+//q4
+for ( let i=5; i>=1; i--)
+{
+    for (let j=5;j>=i;j--)
+    {
+     document.writeln(j + " ")
+    }
+    document.writeln("<br>")
+}
+
+document.writeln("<br>")
+
+//q5
+for ( let i=5; i>=1; i--)
+{
+    for (let j=i;j<=5;j++)
+    {
+     document.writeln(j + " ")
+    }
+    document.writeln("<br>")
+}
+
+document.writeln("<br>")
+
+//q6
+for ( let i=5; i>=1; i--)
+{
+    for (let j=5;j>=i;j--)
+    {
+     document.writeln(i + " ")
+    }
+    document.writeln("<br>")
+}
+
+document.writeln("<br>")
+
+//q7
+for ( let i=5; i>=1; i--)
+{
+    for (let j=1;j<=i;j++)
+    {
+     document.writeln(j + " ")
+    }
+    document.writeln("<br>")
+}
+
+document.writeln("<br>")
+
+//q8
+for ( let i=1; i<=5; i++)
+{
+    for (let j=i;j<=5;j++)
+    {
+     document.writeln(j + " ")
+    }
+    document.writeln("<br>")
+}
+
+document.writeln("<br>")
+
+//q9
+for ( let i=1; i<=5; i++)
+{
+    for (let j=5;j>=i;j--)
+    {
+     document.writeln(i + " ")
+    }
+    document.writeln("<br>")
+}
+
+document.writeln("<br>")
+
+//q10
+for ( let a=1; a<=5; a++)
+{
+    for (let k=5;k>=a;k--)
+    {
+     document.writeln(k + " ")
+    }
+    document.writeln("<br>")
+}
+
+document.writeln("<br>")
+
+//q11
+for ( let i=5; i>=1; i--)
+{
+    for (let j=i;j>=1;j--)
+    {
+     document.writeln(j + " ")
+    }
+    document.writeln("<br>")
+}
+
+document.writeln("<br>")
+
+//q12
+for ( let i=5; i>=1; i--)
+{
+    for (let j=1;j<=i;j++)
+    {
+     document.writeln(i + " ")
+    }
+    document.writeln("<br>")
+}
+
+document.writeln("<br>")
+
+
+//q13
+for ( let i=1; i<=5; i++)
+{
+    for (let j=1;j<=i;j++)
+    {
+     document.writeln("*")
+    }
+    document.writeln("<br>")
+}
+
+document.writeln("<br>")
+
+
+//q14
+for ( let a=1; a<=5; a++)
+{
+    for (let k=5;k>=a;k--)
+    {
+     document.writeln("*")
+    }
+    document.writeln("<br>")
+}
+
+document.writeln("<br>")
+
+//q15
+for(let a=1;a<=5;a++)
+{
+    for(let b=1;b<=a;b++)
+    {
+    if((a+b) % 2 === 0)
+    {
+        document.write("1 ")
+    }
+    else
+    {
+        document.write("0 ")
+    }
+    }
+     document.writeln("<br>")
+}
+document.writeln("<br>")
+
+//q16
+for(let a=5;a>=1;a--)
+{
+    for(let b=1;b<=a;b++)
+    {
+    if((a+b) % 2 === 0)
+    {
+        document.write("1 ")
+    }
+    else
+    {
+        document.write("0 ")
+    }
+    }
+     document.writeln("<br>")
+}
+document.writeln("<br>")
+
+//q17
+// for(let a=1;a<=5;a++)
+// {
+//     for(let b=1;b<=a;b++)
+//     {
+//     if((a+b) % 2 === 0)
+//     {
+//         document.write("1 ")
+//     }
+//     else
+//     {
+//         document.write("0 ")
+//     }
+//     }
+//      document.writeln("<br>")
+// }
+// document.writeln("<br>")
+
+//q18
+// a = 0
+// for(let a=5;a>=1;a--)
+// {
+//     for(let b=1;b<=a;b++)
+//     {
+//     if((a+b) % 2 === 0)
+//     {
+//         document.write("1 ")
+//     }
+//     else
+//     {
+//         document.write("0 ")
+//     }
+//     }
+//      document.writeln("<br>")
+// }
+// document.writeln("<br>")
+
+
+//19
+let num =1
+for(let i=1;i<=5;i++)
+{
+    for(let k=1;k<=i;k++)
+    {
+        document.write(num + " ")
+        num++
+    }
+document.writeln("<br>")
+}
+ document.writeln("<br>")
+
+ //20
+
+ for(let a=1;a<=5;a++)
+{
+    for(let b=1;b<=a;b++)
+    {
+    if((a+b) % 2 === 0)
+    {
+        document.write("- ")
+    }
+    else
+    {
+        document.write("| ")
+    }
+    }
+     document.writeln("<br>")
+}
+document.writeln("<br>")
